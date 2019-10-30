264225bbc35c3352bba9b03327a5a0433ede9ced
Gumatan <arnaut.demaret@gmail.com>
2019-10-30 16:03:16 +0100

    Revert "tableau moviesCategories"
    This reverts commit 7aa7683b6562f1f361b97cbffe6ed27818d4f50b.

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./reset.css";
+import React from "react";
+import Nav from "./Components/Nav";
+import "./App.scss";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Nav />
+      </div>
+    );
+  }
+}
+
+export default App;
