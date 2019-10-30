b6e6bdcaf2f5b3ebf3eb8971face3c74832027ac
blandine <blandine.mallart@gmail.com>
2019-10-30 15:45:16 +0100

    logo nav + scss

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -3,8 +3,14 @@ import "./Nav.scss";
 
 const Nav = () => (
   <nav>
-    {/* <img className="logo" src="logo.jpeg" alt="Crime Advisor logo" /> */}
-    <h1>Crime Advisor</h1>
+    {
+      <img
+        className="logo"
+        src="LogoCrimeAdvisor.png"
+        alt="Crime Advisor logo"
+      />
+    }
+    <h1>Your choice</h1>
   </nav>
 );
 export default Nav;
