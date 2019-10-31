b3bf08f89501635d64f4fa913e849deeedd55eb6
Alice Carrasqueira <maeva.alice.carras@gmail.com>
2019-10-31 10:23:17 +0100

    ajout images et sons transitions

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -7,21 +7,29 @@ class Main extends React.Component {
     this.state = {
       imgs: [
         "./images/thunder.gif",
+        "./images/transparency.png",
         "./images/doll.gif",
+        "./images/transparency.png",
         "./images/lamort.gif",
+        "./images/transparency.png",
         "./images/fille.gif"
       ],
 
       sounds: [
         "./sons/thunder.mp3",
+        "./sons/transition.mp3",
         "./sons/doll.mp3",
+        "./sons/transition.mp3",
         "./sons/crow.mp3",
+        "./sons/transition.mp3",
         "./sons/run.mp3"
       ],
+
       i: 0,
       soundSrc: "./sons/thunder.mp3",
       imgSrc: "./images/thunder.gif"
     };
+
     setInterval(() => {
       this.setState({
         i: this.state.i + 1,
