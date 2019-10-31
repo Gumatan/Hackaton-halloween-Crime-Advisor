66d2041b5df28d5f8fe9200ce15cf14d2789831f
Alice Thammavong <thammavongalice@gmail.com>
2019-10-31 11:20:35 +0100

    Re-ajout du lien vers /dead.

--- src/Components/Slogan.jsx
+++ src/Components/Slogan.jsx
@@ -1,20 +1,23 @@
 import React from "react";
 import "./Slogan.scss";
+import { Link } from "react-router-dom";
 
 function Slogan() {
   return (
-    <div className="main">
-      <img
-        className="logo"
-        src="./images/LogoCrimeAdvisor.png"
-        alt="logo"
-      ></img>
-      <div className="slogan">La mort n'a pas de prix ></div>
-      <div className="text-explicatif">
-        <p>Vous rêvez d'une fin digne d'un film d'horreur? </p>
-        <p>Notre équipe de Death experts vous accompagne.</p>
+    <Link to="/dead">
+      <div className="main">
+        <img
+          className="logo"
+          src="./images/LogoCrimeAdvisor.png"
+          alt="logo"
+        ></img>
+        <div className="slogan">Death is priceless ></div>
+        <div className="text-explicatif">
+          <p>Dream of an horror movie ending? </p>
+          <p>Our Death experts walk you through.</p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
 export default Slogan;
